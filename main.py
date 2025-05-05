4d7a01b21d5a2fe36b0f20a9a7087d5911a6048e
goncalofgalmeida <102439759+goncalofgalmeida@users.noreply.github.com>
2025-05-05 17:42:05 +0100

    Accept different filepaths as inputs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_each_char_count, get_sorted_chars_list
 
 def get_book_text(filepath):
@@ -6,7 +7,11 @@ def get_book_text(filepath):
 		return file_contents
 	
 def main():
-	filepath = "./books/frankenstein.txt"
+	if len(sys.argv) != 2:
+		print("Usage: python3 main.py <path_to_book>")
+		sys.exit(1)
+
+	filepath = sys.argv[1]
 	file_content = get_book_text(filepath)
 	num_words = get_num_words(file_content)
 	found_chars_count = get_each_char_count(file_content)
